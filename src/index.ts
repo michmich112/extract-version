21498eaa20a60028821b3049fe878e23386fee11
michmich112 <cantacuzene.michel@gmail.com>
2021-11-22 23:01:39 -0800

    updated version for github action stats

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,19 @@ import * as core from '@actions/core';
 import { getActionOptions, generateSchemeRegexp, getCurrentVersion } from './utils';
 import collectStats from "gh-action-stats";
 
-const FAILURE = 1;
-
-
 async function main() {
-  collectStats(); // collect the stats of the action run
-  const options = getActionOptions();
-  Object.freeze(options);
-  const schemeRegExp = generateSchemeRegexp(options.schema);
-  const version = await getCurrentVersion(options, schemeRegExp);
-  console.info(`[SUCCESS] - found version ${version}`);
-  return version;
-}
-
-
-main()
-  .then(version => core.setOutput('version', version))
-  .catch(e => {
+  try {
+    const options = getActionOptions();
+    Object.freeze(options);
+    const schemeRegExp = generateSchemeRegexp(options.schema);
+    const version = await getCurrentVersion(options, schemeRegExp);
+    console.info(`[SUCCESS] - found version ${version}`);
+    core.setOutput('version', version)
+  } catch (e) {
     core.error(e);
     core.setFailed(e.message);
-    return FAILURE;
-  });
+    throw e;
+  }
+}
+
+collectStats(main);
